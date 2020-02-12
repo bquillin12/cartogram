21c45a037fd76e72fb4215ac977d5b5cbaadb031
Bryce Quillin <bquillin@yahoo.com>
2020-02-12 13:15:10 -0500

    Update cartogram.R

--- cartogram.R
+++ cartogram.R
@@ -24,22 +24,20 @@ worldR <- readOGR(dsn = getwd(), layer= "TM_WORLD_BORDERS-0.3") # http://themati
 proj4string(worldR)
 datR <- read.csv("population.csv") # scaling variable https://population.un.org/wpp/
 
-
 # Create cartogram --------------------------------------------------------
 
 # Merge SpatialPolygonsDataFrame with other info
-map_dat <- merge(worldR, datR, by.x="ISO3",by.y="iso")
+map_dat <- merge(worldR, datR, by.x="ISO3", by.y="iso")
 
 #remove coordinate reference system arguments
 proj4string(map_dat) <- CRS(as.character(NA)) 
 world.carto <- quick.carto(map_dat, map_dat$pop, blur = 0)
 
-
 # Plot cartogram ----------------------------------------------------------
 
 my.palette = c("skyblue1", "skyblue4", "royalblue2", "blue1", "blue4")
 plot(world.carto) 
-spplot(world.carto, 'pop', col.regions = my.palette, cuts = length(my.palette)-1,main="World Population")
+spplot(world.carto, 'pop', col.regions = my.palette, cuts = length(my.palette)-1, main = "World Population")
 
 
 
