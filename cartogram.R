1e5684510b1d84c840d551f8ed0189e034898c4b
Bryce Quillin <bquillin@yahoo.com>
2020-02-14 08:38:22 -0500

    Update cartogram.R

--- cartogram.R
+++ cartogram.R
@@ -6,7 +6,7 @@
 
 # Set working dir ---------------------------------------------------------
 
-setwd() 
+setwd() # enter your working directory
 
 # Packages ----------------------------------------------------------------
 
@@ -18,24 +18,25 @@ library(getcartr)
 
 # Read data ---------------------------------------------------------------
 
-worldR <- readOGR(dsn = getwd(), layer= "TM_WORLD_BORDERS-0.3") # http://thematicmapping.org/downloads/world_borders.php
-proj4string(worldR)
-datR <- read.csv("population.csv") # scaling variable https://population.un.org/wpp/
+map <- readOGR(dsn = getwd(), layer= "TM_WORLD_BORDERS-0.3") # http://thematicmapping.org/downloads/world_borders.php
+proj4string(map)
+data <- read.csv("population.csv") # scaling variable https://population.un.org/wpp/
 
 # Create cartogram --------------------------------------------------------
 
 # Merge SpatialPolygonsDataFrame with other info
-map_dat <- merge(worldR, datR, by.x="ISO3", by.y="iso")
+map_data <- merge(map, data, by.x="ISO3", by.y="iso")
 
 #remove coordinate reference system arguments
-proj4string(map_dat) <- CRS(as.character(NA)) 
-world.carto <- quick.carto(map_dat, map_dat$pop, blur = 0)
+proj4string(map_data) <- CRS(as.character(NA)) 
+cartogram <- quick.carto(data, data$pop, blur = 0)
 
 # Plot cartogram ----------------------------------------------------------
 
 my.palette = c("skyblue1", "skyblue4", "royalblue2", "blue1", "blue4")
-plot(world.carto) 
-spplot(world.carto, 'pop', col.regions = my.palette, cuts = length(my.palette)-1, main = "World Population")
+plot(cartogram) 
+spplot(cartogram, 'pop', col.regions = my.palette, cuts = length(my.palette)-1, main = "World Population")
 
-
+############################################################################
+############################################################################
 
